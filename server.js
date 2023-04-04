bb1f464f58fcde5a61c1dcab2d6af2f1d0566866
karlnh <hubigkarl@gmail.com>
2023-04-03 23:12:24 -0500

    init server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+  
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+  
