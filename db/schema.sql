ff79f338a4a749790c6d1c2d669d8eb0b9785c47
karlnh <hubigkarl@gmail.com>
2023-04-03 23:55:41 -0500

    add cascade deletion

--- db/schema.sql
+++ db/schema.sql
@@ -14,6 +14,7 @@ CREATE TABLE role (
     salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
+    ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
@@ -22,6 +23,6 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (role_id) REFERENCES role(id)
+    ON DELETE CASCADE
 );
