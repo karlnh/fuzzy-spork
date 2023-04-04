91ed2549103bb70e687f66a919f456c63c366c6e
karlnh <hubigkarl@gmail.com>
2023-04-03 23:13:43 -0500

    add db connection

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+    console.log(`Connected to database.`)
+);
+
 app.use((req, res) => {
     res.status(404).end();
 });
