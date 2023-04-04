bda39fa1c726ebf40a461af029f6ba32dd01f225
karlnh <hubigkarl@gmail.com>
2023-04-03 23:38:42 -0500

    add initial prompt

--- server.js
+++ server.js
@@ -27,3 +27,25 @@ app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
+inq
+    .prompt([
+        {
+            type: 'list',
+            name: 'main',
+            message: 'What would you like to do?',
+            choices: [
+                'View All Employees',
+                'Add Employee',
+                'Update Employee Role',
+                'View All Roles',
+                'Add Role',
+                'View All Departments',
+                'Add Department',
+                'Quit',
+                new inq.Separator(),
+            ],
+        },
+    ])
+    .then((answer) => {
+        console.log(answer.main);
+    });
